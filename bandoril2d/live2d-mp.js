40e334cd9d24d6f1ee1b60e83a1a2aac942f88ca
qq1010903229 <1010903229@qq.com>
2021-04-18 09:51:01 +0800

    commit

---
+++ bandoril2d/live2d-mp.js
@@ -0,0 +1,15 @@
+localStorage.l2dm=localStorage.l2dm || '/w.json';
+function initl2d(){
+$("#live2d").attr("width",300);
+$("#live2d").attr("height",375);
+$(".waifu").css("right",'0px');
+$("#live2d")[0].addEventListener("click",l2dclick);
+loadlive2d('live2d', localStorage.l2dm, null);
+}
+function setl2dm(a){
+loadlive2d('live2d', localStorage.l2dm=a, null);
+}
+function l2dclick(){
+window.l2de.startMotion('c',0);
+}
+setTimeout(initl2d,500);
