e5c9dbae9700dd9c96e8c05854a19a39de535e3d
qq1010903229 <1010903229@qq.com>
2023-03-17 15:05:33 +0800

    commit

--- bandoril2d/live2d-mp.js
+++ bandoril2d/live2d-mp.js
@@ -1,9 +1,13 @@
-if(localStorage.l2dv!='4'){
-	localStorage.l2dv='4';
+if(localStorage.l2dv!='5'){
+	localStorage.l2dv='5';
 	localStorage.l2dmv='0';
+	localStorage.l2dmn='/newkasumilivedefault.json';
 }
+if(document.location.href.startsWith(document.location.origin+"/ksm/index-v7.html?")||document.location.href==document.location.origin+"/ksm/index-v7.html")localStorage.l2dmv='1';
+if(document.location.href.startsWith(document.location.origin+"/ksm/?")||document.location.href==document.location.origin+"/ksm/")localStorage.l2dmv='0';
+if(document.location.href.startsWith(document.location.origin+"/ksm/index.html?")||document.location.href==document.location.origin+"/ksm/index.html")localStorage.l2dmv='0';
 localStorage.l2dm=localStorage.l2dm || '/kasumievent130.json';
-localStorage.l2dmn=localStorage.l2dmn || '/newkasumicasual.json';
+localStorage.l2dmn=localStorage.l2dmn || '/newkasumilivedefault.json';
 function initl2d(){
 $("#live2d").attr("width",600);
 $("#live2d").attr("height",750);
@@ -18,21 +22,22 @@ $(".waifu-tips").css("font-size",'15px');
 $(".waifu-tool").css("font-size",'16px');
 $(".waifu-tool span").css("line-height",'20px');
 $('.waifu-tool .fui-chat').hover(function (){
-	if(document.location.href.startsWith(document.location.origin+"/ksm/index-v7.html?")||document.location.href==document.location.origin+"/ksm/index-v7.html")showMessage('想要了解我的这两个版本之间的不一样的地方吗？',3000);
-	else showMessage('想要了解不一样的我吗？',3000);
+	showMessage('想要了解不一样的我吗？',3000);
 });
 var chat_times=0;
 $('.waifu-tool .fui-chat').click(function (){
 	chat_times++;
-	if(document.location.href.startsWith(document.location.origin+"/ksm/index-v7.html?")||document.location.href==document.location.origin+"/ksm/index-v7.html")showMessage([
-	'你知道吗？我的这两个版本的画风是不一样的（注意看我的这两个版本的眼睛）。',
-	'你知道吗？我的这两个版本都有各自的3D模型，它们是不一样的。',
-	'你知道吗？我的新版本的年龄正好是旧版本的年龄+1岁。',
-	'你知道吗？我的新版本的年级是高三，而旧版本的年级是高二。',
+	if(localStorage.l2dmv=='1')showMessage([
 	'你知道吗？我的新版本将不会再打歌了，因此，loader3229会继续用我的旧版本进行测试自制谱。',
-	'你知道吗？我的新版本除了可以弹吉他之外，还可以打鼓，弹键盘，甚至还可以当DJ？当然，只要有VMD文件，我的旧版本也可以！',
-	//'你知道吗？在3月16日的日服7.0超大型更新之后，我的这两个版本将会共享演出服装。',
-	//'你知道吗？在3月16日的日服7.0超大型更新之后的某个时间点，我的这两个版本将会同时出场。',
+	'你知道吗？我除了可以弹吉他之外，还可以打鼓，弹键盘，甚至还可以当DJ？',
+	'你知道吗？Kirakira就是Kirakira，而Dokidoki就是Dokidoki！',
+	'你知道吗？在我陪着loader3229的时候，我想到了很多的歌词！',
+	'你知道吗？我知道，你已经点了'+chat_times+'次这个按钮。（本计数在刷新或离开页面后重置）',
+	'你知道吗？我有许多不同的形象，包括Live2D版形象，MV版形象，卡面版形象等等。',
+	'你知道吗？现在我和我的旧版本有各自的3D模型。',
+	'你知道吗？在新版的3DMV设置中，可以把原3DMV的任何一个成员替换成我。此时我将会尽量还原这个成员的动作。',
+	'你知道吗？在新版的3DMV中，我有可能会偷懒或者瞬间移动。只要3DMV摄像头拍不到我，我就很有可能会偷懒。',
+	'我要赶紧认真学习，准备高考！',
 	],5000,true);
 	else showMessage([
 	//'你知道吗？我会跳舞！<a href="https://www.bilibili.com/video/BV14S4y177bd" target="_blank">看一下我跳的《千本桜》吧！</a>',
@@ -40,19 +45,17 @@ $('.waifu-tool .fui-chat').click(function (){
 	//'你知道吗？我会跳舞！<a href="https://www.bilibili.com/video/BV1hv4y1i7EM" target="_blank">看一下我跳的《スターナイトスノー》吧！</a>',
 	//'你知道吗？我会跳舞！<a href="https://www.bilibili.com/video/BV1hv4y1i7EM" target="_blank">看一下我跳的《スターナイトスノー》吧！</a>',
 	'你知道吗？我已经打出了《HELL! or HELL?》Special难度的ALL PERFECT！',
+	'你知道吗？我已经打出了《HELL! or HELL?》超难Special难度（33级官谱）的ALL PERFECT！',
 	'你知道吗？我已经打出了《SENSENFUKOKU》Special难度的ALL PERFECT！',
 	'你知道吗？我已经打出了loader3229的<a href="https://bestdori.com/community/charts/73910">qualia -ideaesthesia- [ANOTHER 35]</a>（Bestdori ID 73910）的ALL PERFECT！',
 	'你知道吗？我的手指每秒可以点击屏幕15次，而loader3229的手指每秒可以点击屏幕9.6次。',
 	'你知道吗？我会弹任何型号的吉他，包括RANDOM STAR。',
-	//'你知道吗？把我的Live2D服装切换到国服2021愚人节，再点一次这个按钮，就会发生不同的事情哦！',
 	'你知道吗？Kirakira就是Kirakira，而Dokidoki就是Dokidoki！',
-	//'你知道吗？loader3229正在为我制作一个特别的RANDOM STAR。',
 	'你知道吗？在我陪着loader3229的时候，我想到了很多的歌词！',
 	'你知道吗？我知道，你已经点了'+chat_times+'次这个按钮。（本计数在刷新或离开页面后重置）',
-	'你知道吗？我有许多不同的版本，包括Live2D版，MV版，卡面版，不同的3D版等等。',
-	'你知道吗？现在我有至少2个不同的3D模型，其中有一个3D模型是由タマ制作的旧版本3D模型，而另外一个3D模型是由CraftEgg制作的新版本3D模型（用于我的新版本）（我觉得…我的新版本3D模型…好像有点不好看？）',
+	'你知道吗？我有许多不同的形象，包括Live2D版形象，MV版形象，卡面版形象等等。',
+	'你知道吗？现在我和我的新版本有各自的3D模型，其中新版本的3D模型是由CraftEgg制作的（我觉得…我的新版本3D模型…好像有点不好看？）',
 	'你知道吗？在loader3229的MikuMikuDance 7.39里面，可以同时存在至少10个我。',
-	'你知道吗？<a href="/gt.pdf">这个吉他谱</a>我可以完美弹出来，而其他的香澄应该不行。',
 	],5000,true);
 });
 $('.waifu-tool .fui-star').hover(function (){
@@ -124,7 +127,7 @@ $('.waifu-tool .fui-cmd').click(function (){
 loadlive2d('live2d', localStorage.l2dmv=='0'?localStorage.l2dm:localStorage.l2dmn, null);
 if(document.location.href.startsWith(document.location.origin+"/ksm/?")||document.location.href==document.location.origin+"/ksm/")showMessage('你进入了我的档案页，是想更多的了解我吗？（点击我右边最下面的<span class="fui-cmd"></span>按钮或上面“Live2D模型显示不正常修复”修复我的Live2D模型显示问题）',3000,true);
 else if(document.location.href.startsWith(document.location.origin+"/ksm/index.html?")||document.location.href==document.location.origin+"/ksm/index.html")showMessage('你进入了我的档案页，是想更多的了解我吗？（点击我右边最下面的<span class="fui-cmd"></span>按钮或上面“Live2D模型显示不正常修复”修复我的Live2D模型显示问题）',3000,true);
-else if(document.location.href.startsWith(document.location.origin+"/ksm/index-v7.html?")||document.location.href==document.location.origin+"/ksm/index-v7.html")showMessage('你进入了我的新版本的档案页，是想更多的了解我的新版本吗？（注意：页面右下角Live2D版本的我是旧版本）',3000,true);
+else if(document.location.href.startsWith(document.location.origin+"/ksm/index-v7.html?")||document.location.href==document.location.origin+"/ksm/index-v7.html")showMessage('你进入了我的档案页，是想更多的了解我吗？（点击我右边最下面的<span class="fui-cmd"></span>按钮或上面“Live2D模型显示不正常修复”修复我的Live2D模型显示问题）',3000,true);
 else showMessage('我是闪闪发光、心动不已的户山香澄！而且，因为loader3229，我和其他的户山香澄不一样！（点击我右边最下面的<span class="fui-cmd"></span>按钮或上面“Live2D模型显示不正常修复”修复我的Live2D模型显示问题）',3000,true);
 }
 function setl2dm(a){
@@ -143,7 +146,9 @@ function setl2dmn(a){
 	window.l2da.currentTime=0;
 	window.l2da.pause();
 }
-function setl2dv(a){
+function setl2dv(a){ 
+	$('.waifu-tips').stop().css('opacity',0);
+	sessionStorage.removeItem('waifu-text')
 	localStorage.l2dmv=a;
 	loadlive2d('live2d', localStorage.l2dmv=='0'?localStorage.l2dm:localStorage.l2dmn, null);
 	window.l2da.currentTime=0;
